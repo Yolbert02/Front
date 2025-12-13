f9661aaafc851eb30da02137bca802607815d0ff
Yolbert02 <yolbertmejias05@gmail.com>
2025-12-13 12:06:14 -0400

    arreglo de ruta

--- src/routes.js
+++ src/routes.js
@@ -10,18 +10,18 @@ const Complaints = React.lazy(() => import('./views/complaints/Complaints'))
 const Users = React.lazy(() => import('./views/users/Users'))
 const Profile = React.lazy(() => import('./views/profile/Profile'))
 const Notifications = React.lazy(() => import('./views/notification/Notifications'))
-const Zones = React.lazy(() => import('./views/zones/zones'))
+const Zones = React.lazy(() => import('./views/zones/Zones'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/widgets', name: 'Widgets', element: Widgets },
-  { path: '/Officers', name: 'Officers', element: Officers  },
-  { path: '/Complaints', name: 'Complaints', element: Complaints, exact: true  },
+  { path: '/Officers', name: 'Officers', element: Officers },
+  { path: '/Complaints', name: 'Complaints', element: Complaints, exact: true },
   { path: '/users', name: 'Users', element: Users, exact: true },
   { path: '/profile', name: 'Profile', element: Profile, exact: true },
   { path: '/notification', name: 'Notifications', element: Notifications, exact: true },
-  { path: '/zones', name: 'Zones', element: Zones, exact: true }
+  { path: '/Zones', name: 'Zones', element: Zones, exact: true }
 ]
 
 export default routes
