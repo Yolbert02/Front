b67d92e53af01d1d3600d735246010226b5dc564
Yolbert02 <yolbertmejias05@gmail.com>
2026-02-06 07:26:01 -0400

    RENDER

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const app = require('./src/app');
 
 const PORT = process.env.PORT || 4000;
+const HOST = '0.0.0.0'
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+app.listen(PORT, HOST, () => {
+    console.log(`Servidor corriendo en el puerto http://${HOST}${PORT}`);
     console.log(`Documentación/Estado: http://localhost:${PORT}`);
 });
