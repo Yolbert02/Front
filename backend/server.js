efc052b3b3b0687bb333dfed469c964371e00129
Yolbert02 <yolbertmejias05@gmail.com>
2026-02-06 07:54:20 -0400

    cambios de render

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 4000;
 const HOST = '0.0.0.0'
 
 app.listen(PORT, HOST, () => {
-    console.log(`Servidor corriendo en el puerto http://${HOST}${PORT}`);
-    console.log(`Documentación/Estado: http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+    if (process.env.RENDER_EXTERNAL_URL) {
+        console.log(`URL pública: ${process.env.RENDER_EXTERNAL_URL}`);
+    } else {
+        console.log(`Host: ${HOST}, Port: ${PORT}`);
+    }
 });
